54eb48ff2c1cba187be3abfff6a1988fec7409f3
Shotechs <shotechs@gmail.com>
2016-08-16 01:23:11 -0400

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Write a number for x");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write a number for y");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("///////////////////////");
+            Console.WriteLine("// Choose a option //");
+            Console.WriteLine("///////////////////////");
+            Console.WriteLine("1 - addition");
+            Console.WriteLine("2 - subtration ");
+            Console.WriteLine("3 - multification");
+            Console.WriteLine("4 - division");
+            int z = Convert.ToInt32(Console.ReadLine());
+            switch (z)
+            {
+                case 1:
+                    Console.WriteLine("///////////////////////");
+                    Console.WriteLine("// Result //");
+                    Console.WriteLine("///////////////////////");
+                    Console.WriteLine(x + y);
+                    break;
+                case 2:
+                    Console.WriteLine("///////////////////////");
+                    Console.WriteLine("// Result //");
+                    Console.WriteLine("///////////////////////");
+                    Console.WriteLine(x - y);
+                    break;
+                case 3:
+                    Console.WriteLine("///////////////////////");
+                    Console.WriteLine("// Result //");
+                    Console.WriteLine("///////////////////////");
+                    Console.WriteLine(x * y);
+                    break;
+                case 4:
+                    Console.WriteLine("///////////////////////");
+                    Console.WriteLine("// Result //");
+                    Console.WriteLine("///////////////////////");
+                    Console.WriteLine(x / y);
+                    break;
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
